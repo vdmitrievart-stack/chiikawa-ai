9dd1a30a442d2b0f5c74bbe134ff406b69dcac95
vdmitrievart-stack <vdmitrievart@gmail.com>
2026-04-17 20:34:49 +0300

    Create Level4StorageKeys.js

---
+++ Level4StorageKeys.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  WALLETS: 'wallets',
+  COPYTRADING: 'copytrading',
+  SCORING: 'scoring',
+  POSITIONS: 'positions',
+  EXECUTIONS: 'executions',
+  RISK: 'risk',
+  STRATEGIES: 'strategies',
+  EVENTS: 'events',
+};
