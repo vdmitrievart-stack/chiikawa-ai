1c9c8b1d56565ac81f98fee3bc32b8dc35982e58
vdmitrievart-stack <vdmitrievart@gmail.com>
2026-04-20 03:46:13 +0300

    Create position-service.js

---
+++ telegram-bot/core/position-service.js
@@ -0,0 +1,9 @@
+import {
+  getPositions,
+  getLatestTokenPrice,
+  openPosition,
+  closePosition,
+  markPosition,
+  maybeTakeRunnerPartial,
+  recordTradeOutcomeFromSignalContext
+} from "../scan-engine.js";
